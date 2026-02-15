057eda22f37b2a7c4bf52f8f40f657ebb7fd29fc
Luka Rantalainen <luka.j.rantalainen@gmail.com>
2026-02-15 12:25:50 +0200

    now works with the current format

--- src/data_finder.rs
+++ src/data_finder.rs
@@ -1,15 +1,20 @@
-use std::{fs::File, io::Read, path::Path, time::{SystemTime, UNIX_EPOCH}};
 use serde::{Deserialize, Serialize};
-use url::Url;
 use std::collections::HashMap;
+use std::{
+    fs::File,
+    io::Read,
+    path::Path,
+    time::{SystemTime, UNIX_EPOCH},
+};
+use url::Url;
 
 #[derive(Serialize, Deserialize, PartialEq)]
 enum Rating {
-    #[serde(alias="thumbs_up")]
+    #[serde(alias = "thumbs_up")]
     ThumbsUp,
 
-    #[serde(alias="thumbs_down")]
-    ThumbsDown
+    #[serde(alias = "thumbs_down")]
+    ThumbsDown,
 }
 
 #[derive(Serialize, Deserialize)]
@@ -23,9 +28,8 @@ struct FeedBackJson {
     content: String,
     evaluation_name: Option<String>,
     evaluation_treatment: Option<String>,
-    update_time: String
+    update_time: String,
 }
-
 
 #[derive(Debug, Deserialize, Serialize)]
 #[serde(untagged)]
@@ -85,7 +89,6 @@ pub struct Message {
     pub channel: Option<String>,
 }
 
-
 #[derive(Debug, Serialize, Deserialize)]
 pub struct Author {
     pub role: String,
@@ -93,26 +96,41 @@ pub struct Author {
     pub metadata: HashMap<String, serde_json::Value>,
 }
 
-
-
 #[derive(Debug, Serialize, Deserialize)]
 pub struct MessageContent {
     pub content_type: String,
     pub language: Option<String>,
     pub response_format_name: Option<String>,
-    pub thoughts: Option<Vec<HashMap<String, String>>>,
     pub source_analysis_msg_id: Option<String>,
 
     #[serde(flatten)]
     pub inner: ContentInner,
+    pub thoughts: Option<Vec<Thoughts>>,
 }
 
+#[derive(Debug, Serialize, Deserialize)]
+#[serde(untagged)]
+pub enum Thoughts {
+    Thoughts {
+        thoughts: HashMap<String, String>,
+    },
+    Inner {
+        summary: String,
+        content: String,
+        chunks: Vec<String>,
+        finished: bool,
+    },
+}
 
 #[derive(Debug, Serialize, Deserialize)]
 #[serde(untagged)]
 pub enum ContentInner {
-    TextObject { parts: Vec<String> },
-    TextField { text: String },
+    TextObject {
+        parts: Vec<String>,
+    },
+    TextField {
+        text: String,
+    },
     None {},
     UserEditable {
         user_profile: String,
@@ -125,14 +143,14 @@ pub struct MetadataCitationExtra {
     cited_message_idx: i32,
     search_result_idx: Option<i32>,
     evidence_text: String,
-    cloud_doc_url: Option<String>
+    cloud_doc_url: Option<String>,
 }
 
 #[derive(Debug, Serialize, Deserialize)]
 pub struct MetadataListItem {
-    #[serde(alias="type")]
+    #[serde(alias = "type")]
     metadata_type: String,
-    
+
     title: Option<String>,
     name: Option<String>,
     id: Option<String>,
@@ -140,12 +158,12 @@ pub struct MetadataListItem {
     url: Option<String>,
     text: String,
     pub_date: Option<String>,
-    extra: Option<MetadataCitationExtra>
+    extra: Option<MetadataCitationExtra>,
 }
 #[derive(Debug, Serialize, Deserialize)]
 pub struct CitationMetadata {
     citation_format: HashMap<String, String>,
-    metadata_list: Vec<MetadataListItem>
+    metadata_list: Vec<MetadataListItem>,
 }
 
 #[derive(Debug, Serialize, Deserialize)]
@@ -155,8 +173,8 @@ pub struct MessageMetadata {
 
     #[serde(default)]
     pub user_context_message_data: Option<UserContextData>,
-    
-    #[serde(alias="_cite_metadata")]
+
+    #[serde(alias = "_cite_metadata")]
     pub cite_metadata: Option<CitationMetadata>,
 
     #[serde(flatten)]
@@ -169,13 +187,10 @@ pub struct UserContextData {
     pub about_model_message: Option<String>,
 }
 
-
-
 pub struct Feedback {
     pub positive_amount: i32,
-    pub negative_amount: i32
+    pub negative_amount: i32,
 }
-
 
 pub struct Analysis {
     pub chat_amount: i32,
@@ -191,50 +206,62 @@ pub struct Analysis {
     pub voices_used: Vec<String>,
     pub searched_websites: HashMap<String, i32>,
     pub website_paths: HashMap<String, i32>,
-    pub words: HashMap<String, i32>
+    pub words: HashMap<String, i32>,
 }
 
 pub fn find_feedback(path: &Path) -> Feedback {
-    let mut file = File::open(path.join("message_feedback.json")).expect("Failed to open message_feedback!");
+    let mut file =
+        File::open(path.join("message_feedback.json")).expect("Failed to open message_feedback!");
     let mut content = String::new();
 
-    file.read_to_string(&mut content).expect("Failed to read message_feedback.json content!");
-    let feedbacks: Vec<FeedBackJson> = serde_json::from_str(&content).expect("Invalid message_feedback.json formatting!");
+    file.read_to_string(&mut content)
+        .expect("Failed to read message_feedback.json content!");
+    let feedbacks: Vec<FeedBackJson> =
+        serde_json::from_str(&content).expect("Invalid message_feedback.json formatting!");
 
-
-    return Feedback { 
-        positive_amount: feedbacks.iter().filter(|f| f.rating == Rating::ThumbsUp).count() as i32,
-        negative_amount: feedbacks.iter().filter(|f| f.rating == Rating::ThumbsDown).count() as i32,
-    }
+    return Feedback {
+        positive_amount: feedbacks
+            .iter()
+            .filter(|f| f.rating == Rating::ThumbsUp)
+            .count() as i32,
+        negative_amount: feedbacks
+            .iter()
+            .filter(|f| f.rating == Rating::ThumbsDown)
+            .count() as i32,
+    };
 }
 
 pub fn load_conversations(path: &Path) -> Vec<ConversationRoot> {
-    let mut file = File::open(path.join("conversations.json")).expect("Failed to open conversations!");
+    let mut file =
+        File::open(path.join("conversations.json")).expect("Failed to open conversations!");
     let mut content = String::new();
 
-    file.read_to_string(&mut content).expect("Failed to read conversations.json content!");
-    let conversations: Vec<ConversationRoot> = serde_json::from_str(&content).expect("Invalid conversations.json formatting!");
-    
-    return conversations
+    file.read_to_string(&mut content)
+        .expect("Failed to read conversations.json content!");
+    let conversations: Vec<ConversationRoot> = serde_json::from_str(&content).unwrap();
+
+    return conversations;
 }
 
-
 pub fn analyze_conversations(conversations: Vec<ConversationRoot>) -> Analysis {
-    let mut analysis: Analysis = Analysis { 
+    let mut analysis: Analysis = Analysis {
         chat_amount: conversations.len() as i32,
         messages_from_chatgpt: 0,
         messages_from_user: 0,
         unfinished_messages: 0,
         models_used: HashMap::new(),
         messages_sent: vec![],
-        oldest_message_time: SystemTime::now().duration_since(UNIX_EPOCH).expect("Invalid time stamp").as_millis() as f64,
+        oldest_message_time: SystemTime::now()
+            .duration_since(UNIX_EPOCH)
+            .expect("Invalid time stamp")
+            .as_millis() as f64,
         oldest_message_id: String::new(),
         content_types: HashMap::new(),
         authors: HashMap::new(),
         voices_used: vec![],
         searched_websites: HashMap::new(),
         website_paths: HashMap::new(),
-        words: HashMap::new()
+        words: HashMap::new(),
     };
 
     for conversation in conversations {
@@ -252,17 +279,17 @@ pub fn analyze_conversations(conversations: Vec<ConversationRoot>) -> Analysis {
                 } else if message.author.role == "user" {
                     analysis.messages_from_user += 1;
 
-
                     match &message.content.inner {
                         ContentInner::TextField { text } => {
                             for word in text.split(" ") {
                                 *analysis.words.entry(word.trim().to_string()).or_insert(0) += 1;
                             }
-                        },
+                        }
                         ContentInner::TextObject { parts } => {
                             for sentence in parts {
                                 for word in sentence.split(" ") {
-                                    *analysis.words.entry(word.trim().to_string()).or_insert(0) += 1;
+                                    *analysis.words.entry(word.trim().to_string()).or_insert(0) +=
+                                        1;
                                 }
                             }
                         }
@@ -278,9 +305,12 @@ pub fn analyze_conversations(conversations: Vec<ConversationRoot>) -> Analysis {
                     analysis.unfinished_messages += 1;
                 }
 
-                *analysis.content_types.entry(message.content.content_type).or_insert(0) += 1;
+                *analysis
+                    .content_types
+                    .entry(message.content.content_type)
+                    .or_insert(0) += 1;
 
-                if let Some(slug) = message.metadata.other.get("model_slug"){
+                if let Some(slug) = message.metadata.other.get("model_slug") {
                     *analysis.models_used.entry(slug.to_string()).or_insert(0) += 1
                 }
 
@@ -291,11 +321,17 @@ pub fn analyze_conversations(conversations: Vec<ConversationRoot>) -> Analysis {
                                 continue;
                             }
                             let url = Url::parse(&url).expect(&format!("Invalid url {}", &url));
-                            
+
                             *analysis.website_paths.entry(url.to_string()).or_insert(0) += 1;
-                            *analysis.searched_websites.entry(url.host_str().expect(format!("Invalid URL {}", message.id).as_str()).to_string()).or_insert(0) += 1
+                            *analysis
+                                .searched_websites
+                                .entry(
+                                    url.host_str()
+                                        .expect(format!("Invalid URL {}", message.id).as_str())
+                                        .to_string(),
+                                )
+                                .or_insert(0) += 1
                         }
-                     
                     }
                 }
 
@@ -306,11 +342,10 @@ pub fn analyze_conversations(conversations: Vec<ConversationRoot>) -> Analysis {
                     }
                     analysis.messages_sent.push(time);
                 }
-            
             }
-        analysis.chat_amount += 1;
+            analysis.chat_amount += 1;
         }
     }
 
-    return analysis
+    return analysis;
 }
